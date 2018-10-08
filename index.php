cc4a5dd79187e2243638a73de131197d7a9d2c82
Oniva <blackhoodyrap@gmail.com>
2018-10-08 11:34:34 -0400

    message

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<title>PHP Practice</title>
-
+<head>
+        <title>PHP Practice</title>
+        <link rel="stylesheet" href="index.css">
+</head>
 <div id="centered">
         <form action="login.php" method="post">
                 Name: <input name="name" type="text">
-                <button type="submit" text="Submit">
+                <button type="submit">Submit</button>
         </form>
 </div>
